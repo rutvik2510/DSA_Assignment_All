27473928cd72b96e2bc1d2e90efa9dcbd865b6dd
rutvik2510 <135270820+rutvik2510@users.noreply.github.com>
2024-05-22 11:38:30 +0530

    Add files via upload

---
+++ Assignment-2/Palindrome.js
@@ -0,0 +1,17 @@
+// Palindrome code
+function isPalindrome(str){
+    let rev = "";
+    for(let i = str.length-1 ; i>=0 ; i--){ 
+        rev += str[i];
+    }
+    if(rev == str){
+        return true;
+    } else {
+        return false;
+    }
+} 
+const string1 = isPalindrome("shriya");
+console.log("shriya string is", string1);
+ 
+const string2 = isPalindrome("nitin");
+console.log("nitin string is", string2);
---
+++ Assignment-2/StarPattern.js
@@ -0,0 +1,10 @@
+// Star code
+let rows = 5;
+let pattern = "";
+for(let n=1;n<=rows;n++){
+    for(let num=1;num<=n;num++){
+        pattern += "*";
+    }
+    pattern += ("\n");
+}
+console.log(pattern);  
